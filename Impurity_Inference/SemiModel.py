49655db54e68280d17fa803b21a1be6063acccd4
liuyunho <75022966+liuyunho@users.noreply.github.com>
2022-06-04 13:36:19 +0800

    Add files via upload

---
+++ Impurity_Inference/SemiModel.py
@@ -0,0 +1,11 @@
+import torch
+from CodeScript.ENDECoders import Z2Decoder, XDecoder, Z2LEncoder, Classifier, Z1Encoder
+import pyro
+import torch.nn as nn
+import pyro.distributions as dist
+import pyro.poutine as poutine
+from torch.distributions import constraints
+
+'''Will upload soon
+If there are any questions, please contact yunhe_liu15@fudan.edu.cn
+'''
