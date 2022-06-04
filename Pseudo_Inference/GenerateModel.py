93fcee7f77573ef14142fcd3b33d2fce5c108ed3
liuyunho <75022966+liuyunho@users.noreply.github.com>
2022-06-04 13:37:20 +0800

    Add files via upload

---
+++ Pseudo_Inference/GenerateModel.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+from torch.distributions import constraints
+import pyro
+import pyro.distributions as dist
+from pyro.infer import config_enumerate
+from CodeScript.ENDECoders import zDECODER, ENCODERz, ENCODERd, ENCODERp
+import numpy as np
+
+'''Will upload soon
+If there are any questions, please contact yunhe_liu15@fudan.edu.cn
+'''
---
+++ Pseudo_Inference/__main__.py
@@ -0,0 +1,9 @@
+import argparse
+import numpy as np
+import pandas as pd
+from CodeScript.utils import DataReader
+from CodeScript.Inference import GetPseudo
+
+'''Will upload soon
+If there are any questions, please contact yunhe_liu15@fudan.edu.cn
+'''
